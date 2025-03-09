22414e37f56e71a511a3789c3ae3157ae4fc1f82
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-03-09 03:51:56 +0000

    update] index,

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,13 +6,8 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    if @book.save
-      flash[:notice] = "Successfully created."
-      redirect_to book_path(@book.id)
-    else
-      flash.now[:alert] = "Failed with an error"
-      render :index
-    end
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -24,14 +19,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to '/books'
   end
 
