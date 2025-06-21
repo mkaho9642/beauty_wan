97cc9fce555496ec9ba1b576a39cdca1af4555ef
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-06-21 06:40:17 +0000

    [Add] reviews/new

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,11 +1,20 @@
 class Public::ReviewsController < ApplicationController
   def new
-    @review = PostReview.new
+    @post_review = PostReview.new
   end
 
   def create
+    @post_review = PostReview.new(post_params)
+    @post_review.save
+    redirect_to action: 'index'
   end
 
   def show
   end
+
+
+  private
+    def post_review_params #ストロングパラメータ
+      params.require(:post_review).permit(:review_title, :review_text)
+    end
 end
