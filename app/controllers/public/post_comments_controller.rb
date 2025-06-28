ef7dd601982dbbbdbc0f7f580a20ead8e98b0374
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-06-28 06:31:27 +0000

    [Update] reviews/comments

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,20 +1,20 @@
-class Public::CommentsController < ApplicationController
+class Public::PostCommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    @post_review = PostReview.find(params[:post_review_id])
+    @post_review = PostReview.find(params[:review_id])
     @post_comment = current_user.post_comments.new(post_comment_params.merge(review_id: @post_review.id))
-  end
 
-  if @post_comment.save
-    flash[:notice] = "コメントを投稿しました。"
-    redirect_to review_path(@post_review)
-  else
-    flash.now[:alert] = "コメントの投稿に失敗しました。"
-    @user = @post_review.user
-    @salon = @post_review.salon
-    @new_comment = @post_comment
-    render 'public/reviews/show', status: :unprocessable_entity
+    if @post_comment.save
+      flash[:notice] = "コメントを投稿しました。"
+      redirect_to review_path(@post_review)
+    else
+      flash.now[:alert] = "コメントの投稿に失敗しました。"
+      @user = @post_review.user
+      @salon = @post_review.salon
+      @new_comment = @post_comment
+      render 'public/reviews/show', status: :unprocessable_entity
+    end
   end
 
   def destroy
