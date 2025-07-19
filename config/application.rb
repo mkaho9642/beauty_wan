5ce261a5e2ce39fe522a46c898044e3ac91c151d
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-07-19 06:51:59 +0000

    [Update]config application名変更

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Bookers
+module BeautyWan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
