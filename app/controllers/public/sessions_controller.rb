fe97e270c490b6c3813ab31f204b5a3fce2775c7
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-07-23 13:48:35 +0000

    [Update]sessions

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -5,7 +5,7 @@ class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource)
-    root_path
+    mypage_user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
